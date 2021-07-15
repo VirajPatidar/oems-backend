bd7af843a045e5761d021ef5f093cd0d1e5a378d
Viraj Patidar <vrj381@gmail.com>
2021-07-15 20:38:07 +0530

    quiz urls fixed

--- quiz/urls.py
+++ quiz/urls.py
@@ -12,8 +12,8 @@ from .views import (
 urlpatterns = [
     path('make-quiz/', MakeQuizView.as_view(), name="make-quiz"),
     path('make-question/', CreateQuestionView.as_view(), name="make-question"),
-    path('quiz/<class_id>', GetTeachQuizView.as_view(), name="teach-quiz"),
-    path('quiz/<class_id>/<student_id>', GetStuQuizView.as_view(), name="stu-quiz"),
+    path('<class_id>', GetTeachQuizView.as_view(), name="teach-quiz"),
+    path('<class_id>/<student_id>', GetStuQuizView.as_view(), name="stu-quiz"),
     path('question/<quiz_id>', GetTeachQuestionView.as_view(), name="teach-question"),
     path('question/<quiz_id>/<student_id>', GetStuQuestionView.as_view(), name="stu-question"),
     path('response/<quiz_id>/<student_id>', SubmitQuizResponseView.as_view(), name="quiz-response"),
