3a2272f27f39af774d737df4c70eb42e35e9cbde
Viraj Patidar <vrj381@gmail.com>
2021-07-28 10:28:43 +0530

    CORS setup

--- oems_api/settings.py
+++ oems_api/settings.py
@@ -51,16 +51,22 @@ INSTALLED_APPS = [
     'quiz',
     'sharedfolder',
     'assignment',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'oems_api.urls'
