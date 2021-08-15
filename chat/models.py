1cb64518a16e57a8b4adf12b0fb2a9c38e062fdf
Viraj Patidar <vrj381@gmail.com>
2021-08-15 10:47:47 +0530

    user details added in chat

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from klass.models import Classes
+from authentication.models import User
 
 # Create your models here.
 
@@ -7,7 +8,8 @@ class Chat(models.Model):
     class_id = models.ForeignKey(Classes, on_delete=models.CASCADE, related_name='chat_class_id')
     message = models.CharField(max_length=200, blank=False)
     sent_by = models.CharField(max_length=50, blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat_user_id')
     timestamp = models.DateTimeField(auto_now_add=True)    
 
     def __str__(self):
-        return f"{self.class_id}_____{self.message}_____{self.sent_by}"
+        return f"{self.class_id}_____{self.message}_____{self.user_id}_____{self.sent_by}"
--- chat/serializers.py
+++ chat/serializers.py
@@ -3,6 +3,11 @@ from .models import Chat
 
 class ChatMessageSerializer(serializers.ModelSerializer):
 
+    profile_picture = serializers.SerializerMethodField()
+
     class Meta:
         model = Chat
         fields = '__all__'
+    
+    def get_profile_picture(self, obj):
+        return obj.user_id.avatar.url
